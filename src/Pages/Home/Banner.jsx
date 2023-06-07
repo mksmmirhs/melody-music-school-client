016c35b51363baea74276bb2cf9c9327abea8198
Merajul Islam <meraj.exe@gmail.com>
2023-06-07 15:17:49 +0600

    banner done

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -8,20 +8,36 @@ import banner4 from '../../assets/Home/banner-4.webp';
 
 const Banner = () => {
   return (
-    <Carousel autoPlay={true}>
-      <div>
-        <img src={banner1} />
+    <>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src={banner1} className="w-full" />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img src={banner2} className="w-full" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src={banner3} className="w-full" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src={banner4} className="w-full" />
+        </div>
       </div>
-      <div>
-        <img src={banner2} />
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
       </div>
-      <div>
-        <img src={banner3} />
-      </div>
-      <div>
-        <img src={banner4} />
-      </div>
-    </Carousel>
+    </>
   );
 };
 
