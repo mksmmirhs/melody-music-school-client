8c648a1b47eee4fb1fd11d659be1aecf7c5ce9c5
Merajul Islam <meraj.exe@gmail.com>
2023-06-08 22:33:36 +0600

    home page done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -40,6 +40,60 @@ const Home = () => {
           </div>
         ))}
       </div>
+      <Heading heading="Latest News"></Heading>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8">
+        <div>
+          <h3 className="text-3xl mb-4">International Piano Festival</h3>
+          <p>
+            International Piano Festival Set to Dazzle Music Enthusiasts [City
+            Name] is abuzz with excitement as preparations are in full swing for
+            the highly anticipated International Piano Festival, set to take
+            place from [Dates]. This prestigious event, known for showcasing
+            world-class pianists and celebrating the beauty of piano music,
+            promises to captivate audiences with its exceptional lineup and
+            enchanting performances. Now in its [Xth] edition, the International
+            Piano Festival has firmly established itself as a prominent cultural
+            event on the global music calendar. Renowned pianists from around
+            the world will gather in [City Name] to present a series of
+            recitals, chamber music concerts, and masterclasses, ensuring an
+            unforgettable experience for music enthusiasts.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-3xl mb-4">Columbia Kids Week</h3>
+          <p>
+            International Piano Festival Set to Dazzle Music Enthusiasts [City
+            Name] is abuzz with excitement as preparations are in full swing for
+            the highly anticipated International Piano Festival, set to take
+            place from [Dates]. This prestigious event, known for showcasing
+            world-class pianists and celebrating the beauty of piano music,
+            promises to captivate audiences with its exceptional lineup and
+            enchanting performances. Now in its [Xth] edition, the International
+            Piano Festival has firmly established itself as a prominent cultural
+            event on the global music calendar. Renowned pianists from around
+            the world will gather in [City Name] to present a series of
+            recitals, chamber music concerts, and masterclasses, ensuring an
+            unforgettable experience for music enthusiasts.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-3xl mb-4">Festival of Flutes</h3>
+          <p>
+            International Piano Festival Set to Dazzle Music Enthusiasts [City
+            Name] is abuzz with excitement as preparations are in full swing for
+            the highly anticipated International Piano Festival, set to take
+            place from [Dates]. This prestigious event, known for showcasing
+            world-class pianists and celebrating the beauty of piano music,
+            promises to captivate audiences with its exceptional lineup and
+            enchanting performances. Now in its [Xth] edition, the International
+            Piano Festival has firmly established itself as a prominent cultural
+            event on the global music calendar. Renowned pianists from around
+            the world will gather in [City Name] to present a series of
+            recitals, chamber music concerts, and masterclasses, ensuring an
+            unforgettable experience for music enthusiasts.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
