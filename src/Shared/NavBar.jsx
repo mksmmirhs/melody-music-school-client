927fa2ee8accc02237df384897f5b53d32f3a3f8
Merajul Islam <meraj.exe@gmail.com>
2023-06-13 06:16:52 +0600

    z idex

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -66,7 +66,7 @@ const NavBar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm  z-10 dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               {menu}
             </ul>
